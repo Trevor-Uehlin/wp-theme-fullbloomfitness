a38336bc6913dbae10ae0e97a6a778b1c2203ef5
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2022-03-01 13:37:18 -0800

    woops

--- functions.php
+++ functions.php
@@ -1,14 +1,24 @@
 <?php 
-/* Child theme generated with WPS Child Theme Generator */
+
+##############################################################################################################################
+###################################  ADDING CUSTOM CSS AND JS FILES  #########################################################
+##############################################################################################################################
             
-if ( ! function_exists( 'b7ectg_theme_enqueue_styles' ) ) {            
-    add_action( 'wp_enqueue_scripts', 'b7ectg_theme_enqueue_styles' );
+if ( ! function_exists( 'fbf_add_styles' ) ) {            
+    add_action( 'wp_enqueue_scripts', 'fbf_add_styles' );
     
-    function b7ectg_theme_enqueue_styles() {
+    function fbf_add_styles() {
         wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
         wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ) );
     }
 }
+
+
+
+
+##############################################################################################################################
+################################### SETTING DEFAULT EXCERPT LENGTH  ##########################################################
+##############################################################################################################################
 
 // Over riding the default excerpt length for posts.
 function my_excerpt_length( $length ) {
