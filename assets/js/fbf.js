fdeaa4cee45a2fba525b69a7d547556fdabbb869
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2022-03-01 13:58:28 -0800

    Remove text decoration from site title.

--- assets/js/fbf.js
+++ assets/js/fbf.js
@@ -3,6 +3,6 @@
 console.log("HELLO FROM FBF.JS");
 
 
-var siteTitle = document.getElementsByClassName()[0];
+var siteTitle = document.getElementsByClassName("site-title-parent")[0];
 var a = siteTitle.firstChild;
 a.classList.add("no-text-decoration");
