2d7f68a568b55b3aaf37a4e853b37eaa865c1f55
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2022-03-01 14:04:48 -0800

    Remove text decoration from site title.

--- assets/js/fbf.js
+++ assets/js/fbf.js
@@ -3,7 +3,7 @@
 console.log("HELLO FROM FBF.JS");
 
 
-var siteTitle = document.getElementsByClassName("wp-block-site-title")[0];
+var siteTitle = document.getElementsByClassName("wp-block-site-title");
 console.log(siteTitle);
 var a = siteTitle.firstChild;
 console.log(a)
